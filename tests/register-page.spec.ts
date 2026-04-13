eb9aa3f91967d4ea32b9d1f44149cb20930542f9
Desislava Georgieva -X (desigeor - SOFTSERVE INC at Cisco) <desigeor@cisco.com>
2026-04-14 00:12:16 +0300

    negative tests

--- tests/register-page.spec.ts
+++ tests/register-page.spec.ts
@@ -21,3 +21,33 @@ test('registers with valid credentials', async ({ basePage }) => {
   await expect(registerPage.verificationPopup).toBeVisible();
   await expect(registerPage.verificationPopup).toContainText('Verification code sent');
 });
+
+test('shows error when required fields are empty', async ({ basePage }) => {
+  const registerPage = new RegisterPage(basePage);
+
+  await expect(registerPage.emailInput).toBeFocused();
+  await registerPage.emailInput.press('Tab');
+  await basePage.keyboard.press('Tab');
+
+  await expect(registerPage.requiredFieldError).toHaveCount(2);
+  await expect(registerPage.requiredFieldError).toHaveText(['Required field', 'Required field']);
+});
+
+test('shows error when email is invalid', async ({ basePage }) => {
+  const registerPage = new RegisterPage(basePage);
+
+  await registerPage.fillEmail('invalid-email');
+  await expect(registerPage.requiredFieldError).toHaveText('Invalid e-mail');
+});
+
+test('shows error when leave the checkbox unchecked', async ({ basePage }) => {
+  const registerPage = new RegisterPage(basePage);
+  const email = generateRandomEmail();
+  const password = generateRandomPassword();
+
+  await registerPage.fillEmail(email);
+  await registerPage.fillPassword(password);
+  await registerPage.registerButton.click();
+  await expect(registerPage.requiredFieldError.nth(1)).toBeVisible();
+  await expect(registerPage.requiredFieldError.nth(1)).toHaveText('Required field');
+});
